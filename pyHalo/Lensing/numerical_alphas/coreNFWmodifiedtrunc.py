c8ad61c208a29317ccd185ddb63528df1b99c7de
Daniel Gilman <gilmanda@ucla.edu>
2019-03-18 15:14:21 -0700

    add interpolated cored truncated TNFW routines

--- pyHalo/Lensing/numerical_alphas/coreNFWmodifiedtrunc.py
+++ pyHalo/Lensing/numerical_alphas/coreNFWmodifiedtrunc.py
@@ -46,21 +46,14 @@ class InterpCNFWmodtrunc(object):
     def _get_closest_tau_double(self, tau_value):
 
         minidx = np.argsort(np.absolute(tau_value - self.tau))[0:2]
-        dt1 = np.absolute(self.beta[minidx[0]] - tau_value)
-        w1 = 1-dt1 * self._delta_tau ** -1
-        w2 = 1 - w1
 
-        return w1, w2, minidx
+        return minidx
 
     def _get_closest_beta_double(self, beta_value):
 
         minidx = np.argsort(np.absolute(beta_value - self.beta))[0:2]
 
-        db1 = np.absolute(self.beta[minidx[0]] - beta_value)
-        w1 = 1-db1 * self._delta_beta ** -1
-        w2 = 1 - w1
-
-        return w1, w2, minidx
+        return minidx
 
     def _get_closest_beta(self, beta_value, ind):
 
@@ -68,21 +61,49 @@ class InterpCNFWmodtrunc(object):
 
         return minidx
 
-    def _get_closest(self, tau, beta):
+    def _euclidean(self, xref, yref, xval, yval):
 
-        minidx_beta = np.argsort(np.absolute(beta - self.beta))[0:2]
-        minidx_tau = np.argsort(np.absolute(tau - self.tau))[0:2]
-
-        bval_1, bval_2 = self.beta[minidx_beta[0]], self.beta[minidx_beta[1]]
-        tval_1, tval_2 = self.tau[minidx_tau[0]], self.tau[minidx_tau[1]]
-
-        w1 = 1-np.sqrt((bval_1 - beta) ** 2 * self._delta_beta ** -2 +
-                               (tval_1 - tau) ** 2 * self._delta_tau ** -2)
-        w2 = 1-w1
-
-        return w1, w2, minidx_tau, minidx_beta
+        return ((xref - xval)**2 + (yref - yval)**2)**0.5
 
     def __call__(self, x, y, Rs, r_core, r_trunc, norm):
+
+        R = np.sqrt(x ** 2 + y ** 2)
+        log_xnfw = np.log10(R * Rs ** -1)
+
+        if isinstance(R, float) or isinstance(R, int):
+            if log_xnfw <= self._xmin or log_xnfw >= self._xmax:
+                return 0.
+        else:
+            eps = 1e-5
+            low_inds = np.where(log_xnfw <= self._xmin)
+            high_inds = np.where(log_xnfw >= self._xmax)
+            log_xnfw[low_inds] = self._xmin + eps
+            log_xnfw[high_inds] = self._xmax - eps
+
+        beta = r_core * Rs ** -1
+        tau = r_trunc * Rs ** -1
+
+        tmins = self._get_closest_tau_double(tau)
+        tau_value_1, tau_value_2 = self.tau[tmins[0]], self.tau[tmins[1]]
+
+        bmins = self._get_closest_beta_double(beta)
+        beta_value_1, beta_value_2 = self.beta[bmins[0]], self.beta[bmins[1]]
+
+        dis1 = self._euclidean(tau, beta, tau_value_1, beta_value_1)
+        dis2 = self._euclidean(tau, beta, tau_value_2, beta_value_2)
+        dis3 = self._euclidean(tau, beta, tau_value_1, beta_value_2)
+        dis4 = self._euclidean(tau, beta, tau_value_2, beta_value_1)
+        weight_norm = (dis1 + dis2 + dis3 + dis4) ** -1
+
+        func1 = self.split[tmins[0]][bmins[0]]
+        func2 = self.split[tmins[1]][bmins[1]]
+        func3 = self.split[tmins[0]][bmins[1]]
+        func4 = self.split[tmins[1]][bmins[0]]
+
+        return norm * weight_norm * (dis1 * func1(log_xnfw) + dis2 * func2(log_xnfw) +
+                                     dis3 * func3(log_xnfw) + dis4 * func4(log_xnfw))
+
+    def old__call__(self, x, y, Rs, r_core, r_trunc, norm):
 
         R = np.sqrt(x**2 + y**2)
         log_xnfw = np.log10(R * Rs ** -1)
@@ -108,54 +129,6 @@ class InterpCNFWmodtrunc(object):
 
         return norm * func(log_xnfw)
 
-    def old__call__(self, x, y, Rs, Rc, Rt, norm):
-
-        R = np.sqrt(x**2 + y**2)
-        log_xnfw = np.log10(R * Rs ** -1)
-
-        if isinstance(R, float) or isinstance(R, int):
-            if log_xnfw <= self._xmin or log_xnfw >= self._xmax:
-                return 0.
-        else:
-            eps = 1e-5
-            low_inds = np.where(log_xnfw<=self._xmin)
-            high_inds = np.where(log_xnfw>=self._xmax)
-            log_xnfw[low_inds] = self._xmin + eps
-            log_xnfw[high_inds] = self._xmax - eps
-
-        beta = Rc * Rs ** -1
-        tau = Rt * Rs ** -1
-
-        if beta <= self._betamin or beta >= self._betamax:
-
-            w1, w2, tau_inds = self._get_closest_tau_double(tau)
-
-            beta_ind = self._get_closest_beta(beta, 0)
-
-            func1, func2 = self.split[tau_inds[0]][beta_ind], self.split[tau_inds[1]][beta_ind]
-
-            alpha = w1 * func1(log_xnfw) + w2 * func2(log_xnfw)
-
-        elif tau <= self._tau_min or tau >= self._tau_max:
-
-            w1, w2, beta_inds = self._get_closest_beta_double(beta)
-
-            tau_ind = self._get_closest_tau(tau, 0)
-
-            func1, func2 = self.split[tau_ind][beta_inds[0]], self.split[tau_ind][beta_inds[1]]
-
-            alpha = w1 * func1(log_xnfw) + w2 * func2(log_xnfw)
-
-        else:
-            w1, w2, minidx_tau, minidx_beta = self._get_closest(tau, beta)
-            func1 = self.split[minidx_tau[0]][minidx_beta[0]]
-            func2 = self.split[minidx_tau[1]][minidx_beta[1]]
-            alpha = w1 * func1(log_xnfw) + w2 * func2(log_xnfw)
-
-        alpha[low_inds] = 0
-        alpha[high_inds] = 0
-
-        return norm * alpha
 
 if False:
     c = InterpCNFWmodtrunc()
