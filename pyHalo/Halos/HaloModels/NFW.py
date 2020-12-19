6553f6d36f0bd301e31f0d4ce4993b0bed74df89
Daniel Gilman <gilmanda@ucla.edu>
2020-12-19 02:52:38 -0500

    add TNFW/NFW models

---
+++ pyHalo/Halos/HaloModels/NFW.py
@@ -0,0 +1,154 @@
+from pyHalo.Halos.halo_base import Halo
+from pyHalo.Halos.concentration import Concentration
+import numpy as np
+
+class NFWFieldHalo(Halo):
+
+    def __init__(self, mass, x, y, r3d, mdef, z,
+                 sub_flag, lens_cosmo_instance, args, unique_tag):
+
+        self._lens_cosmo = lens_cosmo_instance
+        self._concentration = Concentration(lens_cosmo_instance)
+
+        super(NFWFieldHalo, self).__init__(mass, x, y, r3d, mdef, z, sub_flag,
+                                            lens_cosmo_instance, args, unique_tag)
+
+    @classmethod
+    def change_profile_definition(cls, halo, new_mdef):
+        """
+
+        :param halo: an instance of Halo with a certain mdef
+        :param new_mdef: a new mass definition
+        :return: a new instance of Halo with the same mass, redshift, and angular position,
+        but a new mass definition.
+        """
+
+        mass = halo.mass
+        x = halo.x
+        y = halo.y
+        r3d = halo.r3d
+        z = halo.z
+        sub_flag = halo.is_subhalo
+        args = halo._args
+        cosmo_m_prof = halo.lens_cosmo
+        unique_tag = halo.unique_tag
+
+        return NFWFieldHalo(mass, x, y, r3d, new_mdef, z,
+                            sub_flag, cosmo_m_prof, args, unique_tag)
+
+    @property
+    def lenstronomy_ID(self):
+        return 'NFW'
+
+    @property
+    def lenstronomy_params(self):
+
+        (concentration) = self.profile_args
+        Rs_angle, theta_Rs = self._lens_cosmo.nfw_physical2angle(self.mass, concentration, self.z)
+
+        x, y = np.round(self.x, 4), np.round(self.y, 4)
+
+        Rs_angle = np.round(Rs_angle, 10)
+        theta_Rs = np.round(theta_Rs, 10)
+
+        kwargs = {'alpha_Rs': theta_Rs, 'Rs': Rs_angle,
+                  'center_x': x, 'center_y': y}
+
+        return kwargs, None
+
+    @property
+    def profile_args(self):
+
+        if not hasattr(self, '_profile_args'):
+            concentration = self._concentration.NFW_concentration(self.mass,
+                                                                  self.z,
+                                                                  self._args['mc_model'],
+                                                                  self._args['mc_mdef'],
+                                                                  self._args['log_m_break'],
+                                                                  self._args['c_scatter'],
+                                                                  self._args['c_scale'],
+                                                                  self._args['c_power'],
+                                                                  self._args['c_scatter_dex'])
+
+            self._profile_args = (concentration)
+
+        return self._profile_args
+
+class NFWSubhhalo(Halo):
+
+    def __init__(self, mass, x, y, r3d, mdef, z,
+                 sub_flag, lens_cosmo_instance, args, unique_tag):
+
+        self._lens_cosmo = lens_cosmo_instance
+        self._concentration = Concentration(lens_cosmo_instance)
+
+        super(NFWSubhhalo, self).__init__(mass, x, y, r3d, mdef, z, sub_flag,
+                                           lens_cosmo_instance, args, unique_tag)
+
+    @classmethod
+    def change_profile_definition(cls, halo, new_mdef):
+
+        """
+
+        :param halo: an instance of Halo with a certain mdef
+        :param new_mdef: a new mass definition
+        :return: a new instance of Halo with the same mass, redshift, and angular position,
+        but a new mass definition.
+        """
+
+        mass = halo.mass
+        x = halo.x
+        y = halo.y
+        r3d = halo.r3d
+        z = halo.z
+        sub_flag = halo.is_subhalo
+        args = halo._args
+        cosmo_m_prof = halo.lens_cosmo
+        unique_tag = halo.unique_tag
+
+        return NFWSubhhalo(mass, x, y, r3d, new_mdef, z,
+                            sub_flag, cosmo_m_prof, args, unique_tag)
+
+    @property
+    def lenstronomy_ID(self):
+        return 'NFW'
+
+    @property
+    def lenstronomy_params(self):
+
+        (concentration) = self.profile_args
+        Rs_angle, theta_Rs = self._lens_cosmo.nfw_physical2angle(self.mass, concentration, self.z)
+
+        x, y = np.round(self.x, 4), np.round(self.y, 4)
+
+        Rs_angle = np.round(Rs_angle, 10)
+        theta_Rs = np.round(theta_Rs, 10)
+
+        kwargs = {'alpha_Rs': theta_Rs, 'Rs': Rs_angle,
+                  'center_x': x, 'center_y': y}
+
+        return kwargs, None
+
+    @property
+    def profile_args(self):
+
+        if not hasattr(self, '_profile_args'):
+            if self._args['evaluate_mc_at_zlens']:
+                z_eval = self.z
+            else:
+                z_eval = self.z_infall()
+
+            concentration = self._concentration.NFW_concentration(self.mass,
+                                                                  z_eval,
+                                                                  self._args['mc_model'],
+                                                                  self._args['mc_mdef'],
+                                                                  self._args['log_m_break'],
+                                                                  self._args['c_scatter'],
+                                                                  self._args['c_scale'],
+                                                                  self._args['c_power'],
+                                                                  self._args['c_scatter_dex'])
+
+            self._profile_args = (concentration)
+
+        return self._profile_args
+
---
+++ pyHalo/Halos/HaloModels/TNFW.py
@@ -0,0 +1,161 @@
+from pyHalo.Halos.halo_base import Halo
+from pyHalo.Halos.concentration import Concentration
+import numpy as np
+
+class TNFWFieldHalo(Halo):
+
+    def __init__(self, mass, x, y, r3d, mdef, z,
+                 sub_flag, lens_cosmo_instance, args, unique_tag):
+
+        self._lens_cosmo = lens_cosmo_instance
+        self._concentration = Concentration(lens_cosmo_instance)
+
+        super(TNFWFieldHalo, self).__init__(mass, x, y, r3d, mdef, z, sub_flag,
+                                           lens_cosmo_instance, args, unique_tag)
+
+    @classmethod
+    def change_profile_definition(cls, halo, new_mdef):
+        """
+
+        :param halo: an instance of Halo with a certain mdef
+        :param new_mdef: a new mass definition
+        :return: a new instance of Halo with the same mass, redshift, and angular position,
+        but a new mass definition.
+        """
+
+        mass = halo.mass
+        x = halo.x
+        y = halo.y
+        r3d = halo.r3d
+        z = halo.z
+        sub_flag = halo.is_subhalo
+        args = halo._args
+        cosmo_m_prof = halo.lens_cosmo
+        unique_tag = halo.unique_tag
+
+        return TNFWFieldHalo(mass, x, y, r3d, new_mdef, z,
+                            sub_flag, cosmo_m_prof, args, unique_tag)
+
+    @property
+    def lenstronomy_ID(self):
+        return 'TNFW'
+
+    @property
+    def lenstronomy_params(self):
+
+        [concentration, rt] = self.profile_args
+        Rs_angle, theta_Rs = self._lens_cosmo.nfw_physical2angle(self.mass, concentration, self.z)
+
+        x, y = np.round(self.x, 4), np.round(self.y, 4)
+
+        Rs_angle = np.round(Rs_angle, 10)
+        theta_Rs = np.round(theta_Rs, 10)
+        r_trunc_arcsec = rt / self._lens_cosmo.cosmo.kpc_proper_per_asec(self.z)
+
+        kwargs = {'alpha_Rs': theta_Rs, 'Rs': Rs_angle,
+                  'center_x': x, 'center_y': y, 'r_trunc': r_trunc_arcsec}
+
+        return kwargs, None
+
+    @property
+    def profile_args(self):
+
+        if not hasattr(self, '_profile_args'):
+
+            truncation_radius = self._lens_cosmo.LOS_truncation_rN(self.mass, self.z,
+                                                             self._args['LOS_truncation_factor'])
+
+            concentration = self._concentration.NFW_concentration(self.mass,
+                                                                  self.z,
+                                                                  self._args['mc_model'],
+                                                                  self._args['mc_mdef'],
+                                                                  self._args['log_m_break'],
+                                                                  self._args['c_scatter'],
+                                                                  self._args['c_scale'],
+                                                                  self._args['c_power'],
+                                                                  self._args['c_scatter_dex'])
+
+            self._profile_args = (concentration, truncation_radius)
+
+        return self._profile_args
+
+class TNFWSubhhalo(Halo):
+
+    def __init__(self, mass, x, y, r3d, mdef, z,
+                 sub_flag, lens_cosmo_instance, args, unique_tag):
+
+        self._lens_cosmo = lens_cosmo_instance
+        self._concentration = Concentration(lens_cosmo_instance)
+
+        super(TNFWSubhhalo, self).__init__(mass, x, y, r3d, mdef, z, sub_flag,
+                                       lens_cosmo_instance, args, unique_tag)
+
+    @classmethod
+    def change_profile_definition(cls, halo, new_mdef):
+
+        """
+
+        :param halo: an instance of Halo with a certain mdef
+        :param new_mdef: a new mass definition
+        :return: a new instance of Halo with the same mass, redshift, and angular position,
+        but a new mass definition.
+        """
+
+        mass = halo.mass
+        x = halo.x
+        y = halo.y
+        r3d = halo.r3d
+        z = halo.z
+        sub_flag = halo.is_subhalo
+        args = halo._args
+        cosmo_m_prof = halo.lens_cosmo
+        unique_tag = halo.unique_tag
+
+        return TNFWSubhhalo(mass, x, y, r3d, new_mdef, z,
+                 sub_flag, cosmo_m_prof, args, unique_tag)
+
+    @property
+    def lenstronomy_ID(self):
+        return 'TNFW'
+
+    @property
+    def lenstronomy_params(self):
+
+        (concentration, rt) = self.profile_args
+        Rs_angle, theta_Rs = self._lens_cosmo.nfw_physical2angle(self.mass, concentration, self.z)
+
+        x, y = np.round(self.x, 4), np.round(self.y, 4)
+
+        Rs_angle = np.round(Rs_angle, 10)
+        theta_Rs = np.round(theta_Rs, 10)
+        r_trunc_arcsec = rt / self._lens_cosmo.cosmo.kpc_proper_per_asec(self.z)
+
+        kwargs = {'alpha_Rs': theta_Rs, 'Rs': Rs_angle,
+                  'center_x': x, 'center_y': y, 'r_trunc': r_trunc_arcsec}
+
+        return kwargs, None
+
+    @property
+    def profile_args(self):
+
+        if not hasattr(self, '_profile_args'):
+            if self._args['evaluate_mc_at_zlens']:
+                z_eval = self.z
+            else:
+                z_eval = self.z_infall()
+
+            truncation_radius = self._lens_cosmo.truncation_roche(self.mass, self.r3d, self._args['RocheNorm'], self._args['RocheNu'])
+            concentration = self._concentration.NFW_concentration(self.mass,
+                                                                  z_eval,
+                                                                  self._args['mc_model'],
+                                                                  self._args['mc_mdef'],
+                                                                  self._args['log_m_break'],
+                                                                  self._args['c_scatter'],
+                                                                  self._args['c_scale'],
+                                                                  self._args['c_power'],
+                                                                  self._args['c_scatter_dex'])
+
+            self._profile_args = (concentration, truncation_radius)
+
+        return self._profile_args
+
