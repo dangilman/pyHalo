38adf95717fc179399d9147f327d5bfacbb868c3
AlexLaroche7 <alexander.laroche@mail.mcgill.ca>
2021-06-28 16:31:56 -0400

    ULDM preset model test function & Zeta cosmo instance fix

--- tests/test_preset_models.py
+++ tests/test_preset_models.py
@@ -1,4 +1,4 @@
-from pyHalo.preset_models import WDMLovell2020, CDM, WDMGeneral, SIDM
+from pyHalo.preset_models import WDMLovell2020, CDM, WDMGeneral, SIDM, ULDM
 import numpy.testing as npt
 import pytest
 
@@ -24,6 +24,11 @@ class TestPresetModels(object):
         realization_SIDM = SIDM(0.5, 1.5, None, None, {}, None, None, None,
                                 None, None, None, sigma_sub=0., LOS_normalization=0.)
         npt.assert_equal(len(realization_SIDM.rendering_classes), 3)
+    
+    def test_ULDM(self):
+
+        realization_ULDM = ULDM(0.5,1.5)
+        npt.assert_equal(len(realization_ULDM.rendering_classes), 3)
 
 if __name__ == '__main__':
      pytest.main()
