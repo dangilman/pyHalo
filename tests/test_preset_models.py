999e08ce99ff843cc63915ed31177111ea8912d4
Daniel Gilman <gilmanda@ucla.edu>
2022-01-06 12:45:56 -0400

    add preset_model_from_name function

--- tests/test_preset_models.py
+++ tests/test_preset_models.py
@@ -1,4 +1,4 @@
-from pyHalo.preset_models import WDM, CDM, SIDM, ULDM
+from pyHalo.preset_models import WDM, CDM, SIDM, ULDM, preset_model_from_name
 import numpy.testing as npt
 import pytest
 
@@ -7,6 +7,9 @@ class TestPresetModels(object):
     def test_CDM(self):
 
         realization_cdm = CDM(0.5, 1.5)
+        npt.assert_equal(len(realization_cdm.rendering_classes), 3)
+        cdm = preset_model_from_name('CDM')
+        realization_cdm = cdm(0.5, 1.5)
         npt.assert_equal(len(realization_cdm.rendering_classes), 3)
 
     def test_WDM(self):
@@ -26,15 +29,29 @@ class TestPresetModels(object):
                               kwargs_suppression_sub=kwargs_suppression_polynomial)
         npt.assert_equal(len(realization_wdm.rendering_classes), 3)
 
+        wdm = preset_model_from_name('WDM')
+        realization_wdm = wdm(0.5, 1.5, 8., suppression_model_field='hyperbolic',
+                              suppression_model_sub='polynomial',
+                              kwargs_suppression_field=kwargs_suppression_hyperbolic,
+                              kwargs_suppression_sub=kwargs_suppression_polynomial)
+        npt.assert_equal(len(realization_wdm.rendering_classes), 3)
+
     def test_SIDM(self):
 
         realization_SIDM = SIDM(0.5, 1.5, None, None, {}, {'x_core_halo': 0.05, 'log_slope_halo': 3.}, None, None,
+                                None, None, None, sigma_sub=0., LOS_normalization=0.)
+        npt.assert_equal(len(realization_SIDM.rendering_classes), 3)
+        sidm = preset_model_from_name('SIDM')
+        realization_SIDM = sidm(0.5, 1.5, None, None, {}, {'x_core_halo': 0.05, 'log_slope_halo': 3.}, None, None,
                                 None, None, None, sigma_sub=0., LOS_normalization=0.)
         npt.assert_equal(len(realization_SIDM.rendering_classes), 3)
 
     def test_ULDM(self):
 
         realization_ULDM = ULDM(0.5,1.5)
+        npt.assert_equal(len(realization_ULDM.rendering_classes), 3)
+        uldm = preset_model_from_name('ULDM')
+        realization_ULDM = uldm(0.5, 1.5)
         npt.assert_equal(len(realization_ULDM.rendering_classes), 3)
 
 if __name__ == '__main__':
