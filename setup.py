81d66f45474d56e7d09864a04d0bef18c20dd3ea
Daniel Gilman <gilmanda@ucla.edu>
2023-10-02 17:32:55 -0500

    update pypi version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [ ]
 
 setup(
     author="Daniel Gilman",
-    version='1.0.0',
+    version='1.0.1',
     author_email='daniel.gilman@utoronto.ca',
     classifiers=[
         'Development Status :: 5 - Stable',
