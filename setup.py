a5b8cab337d8e4726684b19624db14493e69edee
Daniel Gilman <gilmanda@ucla.edu>
2024-09-28 13:19:42 -0500

    update to version 1.3.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [ ]
 
 setup(
     author="Daniel Gilman",
-    version='1.3.1',
+    version='1.3.2',
     author_email='daniel.gilman@utoronto.ca',
     classifiers=[
         #'Development Status :: 5 - Stable',
