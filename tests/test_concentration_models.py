5f6bde16f6755df7ce47faf7ea7067e9fbde118a
Daniel Gilman <gilmanda@ucla.edu>
2024-10-04 16:00:56 -0500

    add Bose2016_ludlow model

--- tests/test_concentration_models.py
+++ tests/test_concentration_models.py
@@ -23,9 +23,11 @@ class TestConcentrationModels(object):
     def setup_method(self):
 
         self.model_list = ['DIEMERJOYCE19', 'PEAK_HEIGHT_POWERLAW', 'WDM_HYPERBOLIC',
-                           'WDM_POLYNOMIAL', 'BOSE2016', 'LAROCHE2022', 'LUDLOW2016', 'LUDLOW_WDM']
+                           'WDM_POLYNOMIAL', 'BOSE2016', 'LAROCHE2022', 'LUDLOW2016', 'LUDLOW_WDM',
+                           'BOSE2016_LUDLOW']
         self.model_names = ['DIEMERJOYCE19', 'PEAK_HEIGHT_POWERLAW', 'WDM_HYPERBOLIC',
-                            'WDM_POLYNOMIAL', 'WDM_POLYNOMIAL', 'WDM_POLYNOMIAL', 'LUDLOW2016', 'LUDLOW_WDM']
+                            'WDM_POLYNOMIAL', 'WDM_POLYNOMIAL', 'WDM_POLYNOMIAL', 'LUDLOW2016', 'LUDLOW_WDM',
+                            'WDM_POLYNOMIAL']
 
     def test_models(self):
 
