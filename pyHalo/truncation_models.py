5eada083be0498f75f6d7b78de12bd0be106a1b0
Daniel Gilman <gilmanda@ucla.edu>
2025-02-18 00:33:14 -0600

    put back mutliple_RS

--- pyHalo/truncation_models.py
+++ pyHalo/truncation_models.py
@@ -1,6 +1,6 @@
 from pyHalo.Halos.tidal_truncation import TruncationRN, TruncationRoche, \
     AdiabaticTidesTruncation, TruncationSplashBack, TruncateMeanDensity, \
-    TruncationGalacticus, ConstantTruncationArcsec, TruncationGalacticusKeeley24
+    TruncationGalacticus, ConstantTruncationArcsec, TruncationGalacticusKeeley24, Multiple_RS
 
 
 def truncation_models(model_name):
@@ -14,7 +14,7 @@ def truncation_models(model_name):
         return TruncationRN, kwargs_model
     elif model_name == 'TRUNCATION_R200':
         kwargs_model['LOS_truncation_factor'] = 200
-        # truncates NFW halos at r200
+        # truncates NFW halos at r50 (for field halos, this is comparable to the splashback radius)
         return TruncationRN, kwargs_model
     elif model_name == 'TRUNCATION_RN':
         return TruncationRN, kwargs_model
@@ -37,6 +37,7 @@ def truncation_models(model_name):
         return TruncationGalacticusKeeley24, kwargs_model
     elif model_name == 'CONSTANT':
         return ConstantTruncationArcsec, kwargs_model
+    elif model_name == 'MULTIPLE_RS':
+        return Multiple_RS, kwargs_model
     else:
         raise Exception('model '+str(model_name)+' not recognized')
-
