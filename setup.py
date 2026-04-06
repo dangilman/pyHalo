7cbd217762f33281cf298ad90c2678c89f75b430
Daniel Gilman <gilmanda@ucla.edu>
2026-04-06 09:24:13 -0500

    update to 1.4.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [ ]
 
 setup(
     author="Daniel Gilman",
-    version='1.4.5',
+    version='1.4.6',
     author_email='gilmanda@uchicago.edu',
     classifiers=[
         #'Development Status :: 5 - Stable',
